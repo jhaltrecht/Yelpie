acb343395593ef9f61ae54c40123c94f8b2388f7
jared <jaredh9@gmail.com>
2020-06-02 12:50:55 -0700

    fixed console errors

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,9 +1,8 @@
 import { combineReducers } from 'redux';
 
 import entities from './entities_reducer';
-import ui from './ui_reducer';
 import session from './session_reducer';
-import errors from './errors_reducer';
+import errors from './session_errors_reducer';
 
 const rootReducer = combineReducers({
     entities,
