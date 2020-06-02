70781d70dd02fb921ff2609bbbddce88a734a611
jared <jaredh9@gmail.com>
2020-06-02 10:36:47 -0700

    initialized all 3 database tables and user model

---
+++ db/migrate/20200602171113_create_businesses.rb
@@ -0,0 +1,9 @@
+class CreateBusinesses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :businesses do |t|
+      t.string :name, null:false
+      t.timestamps
+    end
+        add_index :businesses, :name, unique: true
+  end
+end
