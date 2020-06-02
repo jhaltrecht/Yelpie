83d1b96944bae43d12a14d6e323ef71d489d4a35
jared <jaredh9@gmail.com>
2020-06-02 12:50:55 -0700

    fixed console errors

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,28 +8,28 @@ import {
     HashRouter
 } from 'react-router-dom';
 
-import GreetingContainer from './greeting/greeting_container';
+// import GreetingContainer from './greeting/greeting_container';
 import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_container';
-import SearchContainer from './search/search_container';
-import BenchShowContainer from './bench_show/bench_show_container';
-import BenchFormContainer from './bench_form/bench_form_container';
+// import SearchContainer from './search/search_container';
+// import ReviewShowContainer from './review_show/review_show_container';
+// import ReviewFormContainer from './review_form/review_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
     <div>
         <header>
             <Link to="/" className="header-link">
-                <h1>Bench BnB</h1>
+                <h1>Yelpie</h1>
             </Link>
-            <GreetingContainer />
+            {/* <GreetingContainer /> */}
         </header>
         <Switch>
             <AuthRoute exact path="/login" component={LogInFormContainer} />
             <AuthRoute exact path="/signup" component={SignUpFormContainer} />
-            <ProtectedRoute exact path="/benches/new" component={BenchFormContainer} />
-            <Route path="/benches/:benchId" component={BenchShowContainer} />
-            <Route exact path="/" component={SearchContainer} />
+            {/* <ProtectedRoute exact path="/reviews/new" component={ReviewFormContainer} />
+            <Route path="/reviews/:reviewId" component={ReviewShowContainer} /> */}
+            {/* <Route exact path="/" component={SearchContainer} /> */}
         </Switch>
     </div>
 );
